aaa09325cf08e1b7d1095ab2c09b3b23267da3a9
DhruvAgg03 <55849753+DhruvAgg03@users.noreply.github.com>
2022-07-13 11:52:42 +0530

    Update Universe.hpp

--- headers/Universe.hpp
+++ headers/Universe.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include<stdio.h>
 #include<vector>
-#include "../0_headers/Organism.h"  // :)
 
 namespace namespace_Universe
 {
@@ -19,17 +18,16 @@ namespace namespace_Universe
             std::vector<struct step>moves;
             // need to create an array of organism, need the right scope
             // also need to get the size of the matrix, for now staying 1000x1000 (can malloc also be applied to this environment ??)
-            organism*[1000][1000] environment;
+            //organism*[1000][1000] environment;
         public:
             //Constructor also creates static list of moves
-            // Parameters for constructor -> 
+            // Parameters for constructor ->
             Universe(int maxR);
             void printMoves();
             std::vector<struct step> getMoves();
-            organism* getObject(int posX, int posY); // need to define the scope :)
-            void updateUniverse(int initX,int initY,int finalX, int finalY);
+            //organism* getObject(int posX, int posY); // need to define the scope :)
+            //void updateUniverse(int initX,int initY,int finalX, int finalY);
 
     };
 
 }
-
