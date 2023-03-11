64eba44043da381d83a03836ddba293032f14d61
Johan511 <harihara.sn@gmail.com>
2023-03-11 13:00:01 +0530

    cmake build system added

--- Makefile
+++ Makefile
@@ -1,28 +1,28 @@
-main: Aphrodite.o Traits.o Organism.o Universe.o simulation.o main.o lexer.o parser.o
-	g++ -o main Aphrodite.o Traits.o Organism.o Universe.o simulation.o main.o lexer.o parser.o
-Aphrodite.o: ./Sources/Aphrodite.cpp ./Headers
-	g++ -c -g  ./Sources/Aphrodite.cpp
+# main: Aphrodite.o Traits.o Organism.o Universe.o simulation.o main.o lexer.o parser.o
+# 	g++ -o main Aphrodite.o Traits.o Organism.o Universe.o simulation.o main.o lexer.o parser.o
+# Aphrodite.o: ./Sources/Aphrodite.cpp ./Headers
+# 	g++ -c -g  ./Sources/Aphrodite.cpp
 
-Traits.o: ./Sources/Traits.cpp ./Headers
-	g++ -c -g  ./Sources/Traits.cpp
+# Traits.o: ./Sources/Traits.cpp ./Headers
+# 	g++ -c -g  ./Sources/Traits.cpp
 
-Organism.o: ./Sources/Organism.cpp ./Headers
-	g++ -c -g  ./Sources/Organism.cpp
+# Organism.o: ./Sources/Organism.cpp ./Headers
+# 	g++ -c -g  ./Sources/Organism.cpp
 
-Universe.o: ./Sources/Universe.cpp ./Headers 
-	g++ -c -g  ./Sources/Universe.cpp
+# Universe.o: ./Sources/Universe.cpp ./Headers 
+# 	g++ -c -g  ./Sources/Universe.cpp
 
-simulation.o: ./Sources/simulation.cpp ./Headers
-	g++ -c -g ./Sources/simulation.cpp
+# simulation.o: ./Sources/simulation.cpp ./Headers
+# 	g++ -c -g ./Sources/simulation.cpp
 
-lexer.o: ./Sources/lexer.cpp ./Headers
-	g++ -c -g ./Sources/lexer.cpp
+# lexer.o: ./Sources/lexer.cpp ./Headers
+# 	g++ -c -g ./Sources/lexer.cpp
 
-parser.o: ./Sources/parser.cpp ./Headers
-	g++ -c -g ./Sources/parser.cpp
+# parser.o: ./Sources/parser.cpp ./Headers
+# 	g++ -c -g ./Sources/parser.cpp
 
-main.o: ./Sources/main.cpp
-	g++ -c -g  ./Sources/main.cpp
+# main.o: ./Sources/main.cpp
+# 	g++ -c -g  ./Sources/main.cpp
 
-clean: 
-	rm *.o,*.exe
+# clean: 
+# 	rm *.o,*.exe
--- Sources/simulation.cpp
+++ Sources/simulation.cpp
@@ -2,7 +2,8 @@
 #include "../Headers/Universe.h"
 #include "../Headers/Aphrodite.h"
 
-//Actions in output file
+// Actions in output file
+
 #define MOVE 0
 #define BIRTH 1
 #define SPLIT 2
@@ -11,13 +12,14 @@
 #define Sexual 1
 #define REPRODUCETYPE Sexual
 #define increaseSexualUrge 1
+
 extern std::ofstream outfile;
 extern std::ofstream simulationFile;
 extern int iterNum;
 
 void Universe::run()
 {
-  cout<<"Pc Ic\n";
+  cout << "Pc Ic\n";
   Aphrodite cupid{this};
   ofstream ofile;
   ofile.open("./Output/Output.txt");
@@ -31,7 +33,7 @@ void Universe::run()
     {
       auto currInsect = InsectCopy[i];
       currInsect->update_age(currInsect->get_age() + 1);
-      currInsect->updateSexualUrge(currInsect->getSexualUrge()+increaseSexualUrge);
+      currInsect->updateSexualUrge(currInsect->getSexualUrge() + increaseSexualUrge);
       int x = currInsect->get_x();
       int y = currInsect->get_y();
       auto moves = movesToLocationNew({x, y, 0}, currInsect->giveSpeed(), currInsect->get_vision_radius());
@@ -39,35 +41,39 @@ void Universe::run()
       for (int l = 0; l < moves.size(); l++)
       {
         auto nextObj = getObject(moves[l].x, moves[l].y);
-        
-        if(REPRODUCETYPE==Sexual && Organisms_in_love.find(currInsect)!= Organisms_in_love.end())
+
+        if (REPRODUCETYPE == Sexual && Organisms_in_love.find(currInsect) != Organisms_in_love.end())
         {
-          Insect* interest = Organisms_in_love[currInsect];
+          Insect *interest = Organisms_in_love[currInsect];
           int x_interest = interest->get_x();
           int y_interest = interest->get_y();
-          if(((abs(x - x_interest) == 1) && (abs(y - y_interest) == 0))
-            || ((abs(x - x_interest) == 0) && (abs(y - y_interest) == 1)))
+          if (((abs(x - x_interest) == 1) && (abs(y - y_interest) == 0)) || ((abs(x - x_interest) == 0) && (abs(y - y_interest) == 1)))
           {
             Organisms_in_love.erase(interest);
             Organisms_in_love.erase(currInsect);
-            Insect* baby = cupid.mating(currInsect,interest);
-            if(baby)  { addInsect(baby); }
+            Insect *baby = cupid.mating(currInsect, interest);
+            if (baby)
+            {
+              addInsect(baby);
+            }
           }
         }
 
         moveResult = updateUniverse(x, y, moves[l].x, moves[l].y, outfile);
-        if ( moveResult!= SUCCESS) break;
+        if (moveResult != SUCCESS)
+          break;
         x = moves[l].x;
         y = moves[l].y;
       }
-      simulationFile<<iterNum+1<<","<<currInsect->get_x()<<","<<currInsect->get_y()<<","<<currInsect->get_speciesID()<<","<<currInsect->get_aadhar_number()<<","<<MOVE<<"\n";
-      if(moveResult==DYING_ORGANISM)
-      { 
+      simulationFile << iterNum + 1 << "," << currInsect->get_x() << "," << currInsect->get_y() << "," << currInsect->get_speciesID() << "," << currInsect->get_aadhar_number() << "," << MOVE << "\n";
+      if (moveResult == DYING_ORGANISM)
+      {
         killInsect(currInsect);
       }
       // Asexual Reproduction
-      else{
-        if (REPRODUCETYPE==Asexual && cupid.godSaidYes(currInsect))
+      else
+      {
+        if (REPRODUCETYPE == Asexual && cupid.godSaidYes(currInsect))
         {
           vector<Insect *> daughters = cupid.split(currInsect);
           if (daughters.size() == 2)
